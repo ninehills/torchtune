51b1fca42a4f51345d6caafe75984715e8e9bcbb
Tao Yang <swulling@gmail.com>
2025-11-15 15:08:35 +0800

    fix #2572

--- torchtune/modules/attention_utils.py
+++ torchtune/modules/attention_utils.py
@@ -24,7 +24,8 @@ if _SUPPORTS_FLEX_ATTENTION:
 
     def compile_flex_attention():
         try:
-            return torch.compile(flex_attention)
+            # https://github.com/meta-pytorch/torchtune/issues/2572#issuecomment-2795840762
+            return torch.compile(flex_attention, dynamic=False, mode="max-autotune")
         except Exception as e:
             # It may fail on some combinations of hardware/versions. Using max-autotune fixes this issue.
             # Context: https://github.com/pytorch/torchtune/issues/2113
